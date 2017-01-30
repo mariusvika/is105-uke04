2799f4a3bba3f5aad55bf53c39804636dafc74c7
mariusvika <mariuv16@student.uia.no>
2017-01-30 21:51:20 +0100

    Oppdaterer hello.go

--- hello.go
+++ hello.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, playground")
+	fmt.Println("Hello!")
+  fmt.Println("Using the Log2")
+  fmt.Println("with the base")
+  fmt.Println("in 2, you get the")
+    fmt.Println("Following number:")
 	fmt.Println(math.Log2(2))
 }
